dd23a8ba73edc3fd2afc9c5a1c7dd6b8b2706bef
PeterHuang <peterhuang@inquartik.com>
2022-07-24 23:37:14 +0800

    feat: 編輯內文

--- js/openFile.js
+++ js/openFile.js
@@ -2,15 +2,15 @@
  * 開啟檔案
 */
 function openFile(path, type) {
-  // 沒有 domain 時，按照一般開啟連結的方式
-  const domain = '';
+  // 沒有 網址 時，按照一般開啟連結的方式
+  // const BASE_URL = '';
 
-  // 啟動本地端 Server 時，請將domain 輸入
-  // const domain = 'http://127.0.0.1:5500/';
+  // 啟動本地端 Server 時，請將BASE_URL 輸入
+  const BASE_URL = 'http://127.0.0.1:5500/';
   
   const docType = `ms-${type}`;
-  const fileUrl = `${docType}:ofv|u|${domain}${path}`;
-  if (domain === '') {
+  const fileUrl = `${docType}:ofv|u|${BASE_URL}${path}`;
+  if (BASE_URL === '') {
     window.open(`../${path}`, '_blank');
   } else {
     document.location.href = fileUrl;
