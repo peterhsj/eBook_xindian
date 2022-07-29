0a12048f88255323dc2b04d3a007df6e11e0abd7
PeterHuang <peterhuang@inquartik.com>
2022-07-29 15:12:30 +0800

    fix: 更新網址

--- js/openFile.js
+++ js/openFile.js
@@ -3,10 +3,10 @@
 */
 function openFile(path, type) {
   // 沒有 網址 時，按照一般開啟連結的方式
-  const BASE_URL = '';
+  // const BASE_URL = '';
 
   // 啟動本地端 Server 時，請將BASE_URL 輸入
-  // const BASE_URL = 'http://127.0.0.1:5500/';
+  const BASE_URL = 'http://220.1.117.110/ebook/';
   
   const docType = `ms-${type}`;
   const fileUrl = `${docType}:ofv|u|${BASE_URL}${path}`;
