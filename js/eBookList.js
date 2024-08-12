76ec517e1e57b4f70420febdbe2008f190d4a7f0
peterhsj <peterhsj@gmail.com>
2024-08-12 10:03:22 +0800

    refact: 更新架構

--- js/eBookList.js
+++ js/eBookList.js
@@ -1,155 +1,159 @@
 const menuList = [
   {
-    name: "基礎服務",
+    name: "研究創新",
     subList: [
       {
-        name: "一、服務人員專業度",
+        name: "一、創新提案機制",
         url: "pages/eBook_1_1.html",
       },
       {
-        name: "二、服務設施合宜程度",
+        name: "二、本年度創新作為",
         url: "pages/eBook_1_2.html",
       },
       {
-        name: "三、網站使用便利性",
+        name: "三、歷年創新事項延伸精進",
         url: "pages/eBook_1_3.html",
       },
-      {
-        name: "四、服務資訊透明度",
-        url: "pages/eBook_1_4.html",
-      },
-    ]
+    ],
   },
   {
-    name: "服務遞送",
+    name: "便民服務",
     subList: [
       {
-        name: "一、建置單一窗口整合服務",
+        name: "一、各項地政業務服務流程便捷性",
         url: "pages/eBook_2_1.html",
       },
       {
-        name: "二、減除申辦案件需檢附之書表謄本",
+        name: "二、各項地政業務服務流程透明程度",
         url: "pages/eBook_2_2.html",
       },
       {
-        name: "三、開發線上申辦或跨平台通用服務",
+        name: "三、各項地政業務服務行為親和、專業程度",
         url: "pages/eBook_2_3.html",
-      }
-    ]
-  },
-  {
-    name: "服務量能",
-    subList: [
-      {
-        name: "一、服務精進機制",
-        url: "pages/eBook_3_1.html",
       },
-      {
-        name: "二、突破成長",
-        url: "pages/eBook_3_2.html",
-      },
-      {
-        name: "三、內部簡化作業",
-        url: "pages/eBook_3_3.html",
-      },
-      {
-        name: "四、歷年服務再精進",
-        url: "pages/eBook_3_4.html",
-      }
     ],
-  },
-  {
-    name: "服務評價",
-    subList: [
-      {
-        name: "一、服務特性滿意度調查",
-        url: "pages/eBook_4_1.html",
-      },
-      {
-        name: "二、其他意見調查工具與機制執行情形",
-        url: "pages/eBook_4_2.html",
-      },
-      {
-        name: "三、民眾意見回應處理執行情形",
-        url: "pages/eBook_4_3.html",
-      },
-      {
-        name: "四、內部同仁服務滿意度調查",
-        url: "pages/eBook_4_4.html",
-      }
-    ],
-  },
-  {
-    name: "開放創新",
-    subList: [
-      {
-        name: "一、開放參與",
-        url: "pages/eBook_5_1.html",
-      },
-      {
-        name: "二、創新性",
-        url: "pages/eBook_5_2.html",
-      },
-    ]
-  },
-  {
-    name: "第一線為民服務情形",
-    subList: [
-      {
-        name: "親民服務 - 神秘客查核",
-        url: "pages/eBook_6_1.html",
-      }
-    ]
   },
   {
     name: "地政業務行銷宣導有效程度",
     subList: [
       {
-        name: "一、業務宣導多元管道",
-        url: "pages/eBook_7_1.html",
+        name: "一、行銷方式、對象",
+        url: "pages/eBook_3_1.html",
       },
       {
-        name: "二、有效行銷主題、對象與策略",
-        url: "pages/eBook_7_2.html",
+        name: "二、重點行銷項目及成效",
+        url: "pages/eBook_3_2.html",
       },
-    ]
+    ],
   },
   {
-    name: "電話禮貌測試",
+    name: "整體性施政表現",
     subList: [
       {
-        name: "一、電話禮貌測試績效",
-        url: "pages/eBook_8_1.html",
+        name: "一、整體地政業務施政民眾滿意度",
+        url: "pages/eBook_4_1.html",
       },
       {
-        name: "二、提升服務人員電話禮貌之作法",
-        url: "pages/eBook_8_2.html",
+        name: "二、教育訓練辦理情形",
+        url: "pages/eBook_4_2.html",
       },
-    ]
+      {
+        name: "三、民眾陳情及糾紛案件處理情形",
+        url: "pages/eBook_4_3.html",
+      },
+      {
+        name: "四、輿情反應及處理情形",
+        url: "pages/eBook_4_4.html",
+      },
+      {
+        name: "五、第一線為民服務情形",
+        url: "pages/eBook_4_5.html",
+      },
+      {
+        name: "六、電話禮貌測試",
+        url: "pages/eBook_4_6.html",
+      },
+      {
+        name: "七、政策配合情形",
+        url: "pages/eBook_4_7.html",
+      },
+      {
+        name: "八、上次督導事項",
+        url: "pages/eBook_4_8.html",
+      },
+    ],
   },
-  {
-    name: "綜合業務",
-    subList: [
-      {
-        name: "一、上次督導建議改進事項辦理情形",
-        url: "pages/eBook_9_1.html",
-      },
-      {
-        name: "二、機關內部教育訓練（講習）辦理情形",
-        url: "pages/eBook_9_2.html",
-      },
-      {
-        name: "三、外部教育訓練之回饋機制及辦理情形",
-        url: "pages/eBook_9_3.html",
-      },
-      {
-        name: "四、政策配合情形",
-        url: "pages/eBook_9_4.html",
-      },
-      {
-        name: "五、新聞輿情反應及處理情形",
-        url: "pages/eBook_9_5.html",
-      },
-    ]
-  },
-]
+  //{
+  //  name: "開放創新",
+  //  subList: [
+  //    {
+  //      name: "一、開放參與",
+  //      url: "pages/eBook_5_1.html",
+  //    },
+  //    {
+  //      name: "二、創新性",
+  //      url: "pages/eBook_5_2.html",
+  //    },
+  //  ],
+  //},
+  //{
+  //  name: "第一線為民服務情形",
+  //  subList: [
+  //    {
+  //      name: "親民服務 - 神秘客查核",
+  //      url: "pages/eBook_6_1.html",
+  //    },
+  //  ],
+  //},
+  //{
+  //  name: "地政業務行銷宣導有效程度",
+  //  subList: [
+  //    {
+  //      name: "一、業務宣導多元管道",
+  //      url: "pages/eBook_7_1.html",
+  //    },
+  //    {
+  //      name: "二、有效行銷主題、對象與策略",
+  //      url: "pages/eBook_7_2.html",
+  //    },
+  //  ],
+  //},
+  //{
+  //  name: "電話禮貌測試",
+  //  subList: [
+  //    {
+  //      name: "一、電話禮貌測試績效",
+  //      url: "pages/eBook_8_1.html",
+  //    },
+  //    {
+  //      name: "二、提升服務人員電話禮貌之作法",
+  //      url: "pages/eBook_8_2.html",
+  //    },
+  //  ],
+  //},
+  //{
+  //  name: "綜合業務",
+  //  subList: [
+  //    {
+  //      name: "一、上次督導建議改進事項辦理情形",
+  //      url: "pages/eBook_9_1.html",
+  //    },
+  //    {
+  //      name: "二、機關內部教育訓練（講習）辦理情形",
+  //      url: "pages/eBook_9_2.html",
+  //    },
+  //    {
+  //      name: "三、外部教育訓練之回饋機制及辦理情形",
+  //      url: "pages/eBook_9_3.html",
+  //    },
+  //    {
+  //      name: "四、政策配合情形",
+  //      url: "pages/eBook_9_4.html",
+  //    },
+  //    {
+  //      name: "五、新聞輿情反應及處理情形",
+  //      url: "pages/eBook_9_5.html",
+  //    },
+  //  ],
+  //},
+];
