255a3806732b543604187dc580531f70998abc1d
peterhsj <peterhsj@gmail.com>
2024-08-27 11:55:20 +0800

    fix:更新js

--- js/openFile.js
+++ js/openFile.js
@@ -1,20 +1,19 @@
 /**
  * 開啟檔案
-*/
+ */
 function openFile(path, type) {
   // 沒有 網址 時，按照一般開啟連結的方式
   // const BASE_URL = '';
 
   // 啟動本地端 Server 時，請將BASE_URL 輸入
-  // const BASE_URL = 'http://220.1.117.110/ebook/';
-  const BASE_URL = 'http://127.0.0.1:5500/';
-  
+  const BASE_URL = "http://220.1.117.110/ebook/";
+  //const BASE_URL = 'http://127.0.0.1:5500/';
+
   const docType = `ms-${type}`;
   const fileUrl = `${docType}:ofv|u|${BASE_URL}${path}`;
-  if (BASE_URL === '') {
-    window.open(`../${path}`, '_blank');
+  if (BASE_URL === "") {
+    window.open(`../${path}`, "_blank");
   } else {
     document.location.href = fileUrl;
   }
-
 }
